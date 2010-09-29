f77a5ef3462831fd58ed2c924fba2e418fd1e9ce
Scott Shanks <Scott.Shanks@emerson.com>
2010-09-29 10:42:47 -0400

    Changed to accept arguments from the command line.

--- BACnetPoller/bacnet_bulk.rb
+++ BACnetPoller/bacnet_bulk.rb
@@ -1,4 +1,23 @@
+# == Synopsis
+#
+# bacnet_bulk: performs a bulk set of bacnet queries from the supplied
+# spreadsheet and creates a time stamped copy of that spreadsheet with results
+#
+# == Usage
+#
+# bacnet_bulk ip_address path_to_spreadsheet
+#
+require 'rdoc/usage'
 require 'bacnet_poller'
 
-test = BacnetPoller.new('126.4.202.195', 'C:\LMG_Test\ruby\Utilities\BACnetPoller\BACnet_SC_fdm_29.xls')
-test.run
+# Verify arguments
+if ARGV.length != 2
+  RDoc::usage
+end
+
+# Handle arguments
+ip_address = ARGV.shift
+path_to_spreadsheet = ARGV.shift
+
+bacnet_bulk = BacnetPoller.new(ip_address, path_to_spreadsheet)
+bacnet_bulk.run
