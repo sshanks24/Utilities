7e0d8d0da7763dae8beafc676b3ae806f73b09af
Scott Shanks <Scott.Shanks@emerson.com>
2010-09-29 12:50:52 -0400

    Accepts command line arguments (initial commit)

---
+++ ModbusPoller/modbus_bulk.rb
@@ -0,0 +1,31 @@
+# == Synopsis
+#
+# modbus_bulk: performs a bulk set of modus queries from the supplied
+# spreadsheet and creates a time stamped copy of that spreadsheet with results
+#
+# == Usage
+#
+# modbus_bulk ip_address|comport path_to_spreadsheet
+#
+
+require 'modbus_poller'
+
+# Verify arguments
+if ARGV.length != 3
+  RDoc::usage
+end
+
+# Handle arguments
+interface = ARGV.shift
+path_to_spreadsheet = ARGV.shift
+
+if interface =~ /\d+\.\d+\.\d+\.\d+/ #Not a perfect regex, but good enough
+  modbus_bulk = ModbusPollerTCP.new(1, interface, path_to_spreadsheet, ModbusPoller::PATH_TO_MODPOLL,'')
+  modbus_bulk.run
+elsif interface =~ /com\d+/i
+  modbus_bulk = ModbusPoller485.new(1, interface, path_to_spreadsheet, ModbusPoller::PATH_TO_MODPOLL)
+  modbus_bulk.run
+else 
+  RDoc::usage
+end
+
