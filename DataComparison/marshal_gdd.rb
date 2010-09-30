b801a055ef933fb6283fab1835e9721e427e2516
Scott Shanks <Scott.Shanks@emerson.com>
2010-09-30 12:09:06 -0400

    Added fdm to marshalled data

--- DataComparison/marshal_gdd.rb
+++ DataComparison/marshal_gdd.rb
@@ -1,7 +1,10 @@
 require 'gdd'
+require 'fdm'
 
 gdd = Gdd.new('C:\fdm\enp2dd.xml')
+fdm = Fdm.new('C:\fdm\BDSU.xml')
 
+gdd.merge_hashes_with_fdm(fdm)
 #serialization
 File.open("C:/fdm/hashes","wb") do |file|
    Marshal.dump(gdd,file)
